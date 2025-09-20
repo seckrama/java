98cab4c6f3f136b4080e66f333f39bb77edf2ffb
seckrama <seckrama022@gmail.com>
2025-09-20 12:32:26 +0000

    jenkinsss

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello world! ht This weddb application created uisng java spring boot framework and depolyed in kubernetes .";
+    return "Hello world! ht This weddb application cffreated uisng java spring boot framework and depolyed in kubernetes .";
   }
 }
